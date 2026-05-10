4ac6758e6e16d4e1f12d7bfb28d70721f1f29249
utubefarsiclips-star <utubefarsiclips@gmail.com>
2026-05-10 18:28:30 +0330

    Delete devcontainer/  └── start.sh

--- devcontainer/  └── start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo /usr/local/bin/xray run -c /etc/xray/g2ray.json &>/tmp/xray.log &
-sleep 2
-show-link.sh
-echo "[g2ray] Keep-alive active..."
-while true; do sleep 300; echo "[$(date '+%H:%M')] alive" >>/tmp/keepalive.log; done
